a3ed285ad73e53562a35f8d01dc721c83a31904f
VenJin <venjin.khoo@nanominds-lab.com>
2023-01-04 14:51:10 +0800

    Added Color Template
    Created Color Template and Moved Assets into App

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>START HERE!</Text>
---
+++ frontend/app/config/colors.js
@@ -0,0 +1,17 @@
+export default {
+    primary: "#203665",
+    secondary: "#8047CF",
+    black: "#000",
+    white: "#fff",
+    red: "#FF2730",
+    green: "#4FD966",
+    pink: "#FC00FF59",
+    fade_red: "#DE8F85",
+    primary_grey: "#51565F",
+    secondary_grey: "#1C3B6B24",
+    tertiary_grey: "#8B909D",
+    primary_yellow: "#FFF35E",
+    secondary_yellow: "#FFB155",
+    primary_teal: "#06FDF4",
+    secondary_teal: "#00C5BD"
+}
