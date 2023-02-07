f84e439fa43ca9ea34d55ae53690615148e2cb06
VenJin <venjin.khoo@nanominds-lab.com>
2023-02-07 17:58:03 +0800

    Added AppCard Component
    Created AppCard Component

---
+++ frontend/app/components/AppCard.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import AppText from './AppText';
+import defaultStyles from '../config/styles';
+
+function AppCard({
+    containerStyle,
+    icon,
+    iconColor = defaultStyles.colors.white,
+    subTitle,
+    subtitleStyle,
+    title,
+    titleStyle,
+    size = 40
+}) {
+    return (
+        <View style={[ styles.container, containerStyle ]}>
+            <View style={ styles.titleContainer }>
+                <AppText style={[ styles.text, titleStyle ]}>{ title }</AppText>
+                { subTitle && <AppText style={[ styles.text, subtitleStyle ]}>{ subTitle }</AppText> }
+            </View>
+            { icon && <View style={ styles.iconContainer }>
+                <MaterialCommunityIcons 
+                    name={ icon }
+                    size={ size }
+                    color={ iconColor }
+                />
+            </View>}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: defaultStyles.colors.secondary_yellow,
+        borderRadius: 15,
+        elevation: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 20,
+        shadowColor: defaultStyles.colors.black,
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+        width: '45%',
+    },
+    titleContainer: {
+        alignItems: 'flex-start',
+        justifyContent: 'center'
+    },
+    iconContainer: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        color: defaultStyles.colors.white,
+        fontWeight: '900'
+    }
+})
+
+export default AppCard;
