2d2483cc7fa49405e73bda1258f58b19926517f0
VenJin <venjin.khoo@nanominds-lab.com>
2023-02-07 14:55:12 +0800

    Added Form Button
    Created Form Button

---
+++ frontend/app/components/forms/AppFormButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { useFormikContext } from 'formik';
+
+import AppButton from '../AppButton';
+
+function AppFormButton({ title }) {
+    const { handleSubmit } = useFormikContext();
+
+    return (
+        <AppButton title={ title } onPress={ handleSubmit } />
+    );
+}
+
+export default AppFormButton;
