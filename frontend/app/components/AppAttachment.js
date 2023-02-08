edb820b4e11cda8c5e1396577a14f3749636429a
VenJin <venjin.khoo@nanominds-lab.com>
2023-02-08 12:59:24 +0800

    Added AppAttachment Component
    Created AppAttachment Component

---
+++ frontend/app/components/AppAttachment.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import AppImage from './AppImage';
+import defaultStyles from '../config/styles';
+
+function AppAttachment({
+// General
+    image,
+// Image
+    border,
+    height = 150,
+    style,
+    width = 90,
+// Icon
+    backgroundColor = defaultStyles.colors.white,
+    color = defaultStyles.colors.primary,
+    name,
+    size = 50,
+// Others
+    ...otherProps
+}) {
+    const borderRadius = Math.ceil(size*0.15);
+    const iconSize = Math.ceil(size*0.9);
+
+    return (
+        <View>
+            { image && <AppImage 
+                border={ border }
+                height={ height }
+                image={ image }
+                style={ style }
+                width={ width }
+                {...otherProps}
+            /> }
+            { !image && <View 
+                style={[ 
+                    styles.container,
+                    { backgroundColor },
+                    { height: size, width: size },
+                    border && { borderRadius },
+                    style
+                ]}
+                {...otherProps}
+            >
+                <MaterialCommunityIcons 
+                    name={ name }
+                    color={ color }
+                    size={ iconSize }
+                />
+            </View> }
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        justifyContent: "center"
+    }
+})
+
+export default AppAttachment;
