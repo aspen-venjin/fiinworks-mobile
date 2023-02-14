ba651c13dd86a3c325fc73044907120852adeeaf
VenJin <venjin.khoo@nanominds-lab.com>
2023-02-14 11:42:06 +0800

    Updated Styles
    Added for Pie Chart Stylings

--- frontend/app/config/styles.js
+++ frontend/app/config/styles.js
@@ -14,5 +14,11 @@ export default {
     size: {
         height: Dimensions.get("window").height,
         width: Dimensions.get("window").width
+    },
+    pie: {
+        cornerRadius: 5,
+        innerRadius: 75,
+        labelRadius: 1.65,
+        radius: Dimensions.get("window").width * 0.32
     }
 }
