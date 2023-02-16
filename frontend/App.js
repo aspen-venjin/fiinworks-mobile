062d0ac4e53827c76f2d2f11bfff27a98e07cc82
jyaspspe <124225467+jyaspspe@users.noreply.github.com>
2023-02-16 09:37:16 +0800

    2

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import PenReq from './app/screens/PenReq';
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>START HERE!</Text>
+      <PenReq/>
       <StatusBar style="auto" />
     </View>
   );
