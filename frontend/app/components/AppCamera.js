a47914a5400154421acd1b015b77050975b17f11
VenJin <venjin.khoo@nanominds-lab.com>
2023-03-01 14:12:03 +0800

    Updated AppCamera Component
    Added style and setCamera props, to allow user customize design and to get cameraRef

--- frontend/app/components/AppCamera.js
+++ frontend/app/components/AppCamera.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 
 import { Camera, CameraType } from 'expo-camera';
 
 function AppCamera({
     children,
+    setCamera,
+    style,
     type = CameraType.back
 }) {
     return (
         <Camera
+            style={[ styles.container, style ]}
             type={ type }
+            ref={ ref => setCamera(ref) }
         >
             { children }
         </Camera>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
 
 export default AppCamera;
