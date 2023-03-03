4c65e9f52561b890020dd9c9cf7f03894c9836ef
VenJin <venjin.khoo@nanominds-lab.com>
2023-03-03 14:25:25 +0800

    Updated map function
    Added getIdentifier, defaultRegion, and midValleyRegion

--- frontend/app/functions/map.js
+++ frontend/app/functions/map.js
@@ -1,5 +1,26 @@
 const getDecimalFromPercentage = ( value ) => parseInt(value.slice(0, -1))/100
 
+const getIndentifier = ( arr ) => arr.map( item => item.identifier )
+
+// MALAYSIA REGION
+const defaultRegion = {
+    latitude: 4.0080,
+    longitude: 102.1811,
+    latitudeDelta: 3.700,
+    longitudeDelta: 3.700
+}
+
+// MID VALLEY REGION
+const midValleyRegion = {
+    latitude: 3.1175,
+    longitude: 101.6774,
+    latitudeDelta: 0.050,
+    longitudeDelta: 0.050
+}
+
 export{
-    getDecimalFromPercentage
+    defaultRegion,
+    getDecimalFromPercentage,
+    getIndentifier,
+    midValleyRegion
 }
